7080ec8b37c6ce43989e4b260e0440a96d69e1a6
YueLu0116 <bit_drinkwater@163.com>
2021-08-01 23:47:38 +0800

    update to dialogs

---
+++ OpenFiles/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace OpenFiles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnbtnOpenfileClicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = @"D:\openSourceProject\GUI\WPF\HelloWPF";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach(var filename in openFileDialog.FileNames)
+                {
+                    ltbFiles.Items.Add(System.IO.Path.GetFileName(filename));
+                }
+            }
+        }
+    }
+}
